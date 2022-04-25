f209c15e0733271dba119a02508e09e686908762
Ethan Mosbaugh <ethan@replicated.com>
2022-04-25 12:59:11 -0700

    revert

--- replicated-gcommands/replicated-gcommands.plugin.zsh
+++ replicated-gcommands/replicated-gcommands.plugin.zsh
@@ -4,12 +4,12 @@
 # GPREFIX=
 
 if (( ! ${+commands[gcloud]} ));then
-  print "gcloud not installed, not loading replicated-gcommands plugin"
+  >&2 echo"gcloud not installed, not loading replicated-gcommands plugin"
   return 1
 fi
 
 genv() {
-  echo "Configuration: $(cat ~/.config/gcloud/active_config)"
+  >&2 echo"Configuration: $(cat ~/.config/gcloud/active_config)"
 }
 
 glist() {
@@ -37,13 +37,14 @@ gcreate() {
     instance_names=($(echo ${instance_names} | sed "s/[^ ]* */${GPREFIX}-&/g"))
   fi
   (set -x; gcloud compute instances create ${instance_names[@]} \
-    --labels owner="${GUSER}" \
-    --machine-type=n1-standard-4 \
+    --labels owner="${GUSER}",email="${GUSER}__64__replicated__46__com" \
+    --machine-type=n1-standard-8 \
     --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE \
     --service-account="${default_service_account}" \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
     --image="${image_name}" --image-project="${image_project}" \
-    --boot-disk-size=200GB --boot-disk-type=pd-standard \
+    --boot-disk-size=200GB --boot-disk-type=pd-ssd \
+    --create-disk size=100GB,type=pd-ssd,auto-delete=yes \
     --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any)
 }
 
@@ -99,7 +100,7 @@ gssh-forward() {
   local natip=$(gcloud compute instances describe "${instance_name}" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
   local ip=$(gcloud compute instances describe "${instance_name}" --format="value(networkInterfaces[0].networkIP)")
   # gcloud compute ssh --tunnel-through-iap "${instance_name}" -- -L 8800:$address:8800 -L 8888:$address:8888
-  ssh -L 31880:$ip:8800 -L 31888:$ip:8888 $natip
+  ssh -L 8800:$ip:8800 -L 8888:$ip:8888 $natip
 }
 
 gssh() {
